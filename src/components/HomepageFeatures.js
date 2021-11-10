3b9469d72380d28f71d697ee989d3ac300a8d658
Wille Faler <wille.faler@gmail.com>
2021-11-10 21:11:09 +0100

    home page start

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,45 +4,47 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Focus on Architecture, Not Infrastructure',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'What xlrte is, in user story form ➡️ ',
+    // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Tell xlrte the services you run &amp; 
-        the dependencies they need (databases, message topics etc), 
-        xlrte figures out the underlying infrastructure, networking &amp; IAM needed for you.
+As a developer,
+
+I want to be able to say <i>"I want to deploy a service that uses a database, a block storage bucket & publishes messages to a topic while listening to another"</i>,<br/>
+without having to figure out IAM permissions, network setup, secrets management, initialization order of resources and the dozens of other infrastructure configuration issues that arise.
+
       </>
     ),
   },
-  {
-    title: 'Build Your Own PaaS',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        xlrte comes with batteries included, but is a use-case, service or resource-type you need not supported? No problem! 
-        We provide an extensible API that makes it easy to write your own providers. Contributions are welcome!
-      </>
-    ),
-  },
-  {
-    title: 'Google Cloud Enabled (AWS coming soon)',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        GCP-based infrastructure with a focus on Serverless building blocks is well-supported (CloudRun, Cloud Storage, Pub/Sub, CloudSQL, Secret Manager).<br/>
-        AWS Serverless support is coming soon.
-      </>
-    ),
-  },
+  // {
+  //   title: 'Build Your Own PaaS',
+  //   Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       xlrte comes with batteries included, but is a use-case, service or resource-type you need not supported? No problem! 
+  //       We provide an extensible API that makes it easy to write your own providers. Contributions are welcome!
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Google Cloud Enabled (AWS coming soon)',
+  //   Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       GCP-based infrastructure with a focus on Serverless building blocks is well-supported (CloudRun, Cloud Storage, Pub/Sub, CloudSQL, Secret Manager).<br/>
+  //       AWS Serverless support is coming soon.
+  //     </>
+  //   ),
+  // },
 
 ];
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
+    <div className={clsx('col col--12')}>
+      {/* <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
